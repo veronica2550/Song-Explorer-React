3b0eff023b09b06ac64c8ba10fe66994bd37d199
veronica2550 <minkyoung2550@gmail.com>
2024-04-16 20:49:20 +0900

    API 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,14 @@
 import './App.css'
 import SongList from './components/SongList'
 import SearchBar from './components/SearchBar'
+import { useState } from 'react'
 
-const data = [{
-    "brand": "joysound",
-    "no": "916281",
-    "title": "(NOT JUST) KNEE DEEP",
-    "singer": "FUNKADELIC",
-    "composer": "G.CLINTON,JR.",
-    "lyricist": "G.CLINTON,JR.",
-    "release": "2012-06-18"
-  },
-  {
-    "brand": "joysound",
-    "no": "227531",
-    "title": "(This is not just)Song for me",
-    "singer": "Syrup16g",
-    "composer": "Takashi Igarashi",
-    "lyricist": "Takashi Igarashi",
-    "release": "2005-11-10"
-  },
-  {
-    "brand": "joysound",
-    "no": "239259",
-    "title": "4 MY PEOPLE(BASEMENT JAXX REMIX)",
-    "singer": "MISSY ELLIOTT",
-    "composer": "ELLIOTT MELISSA A,JEFFERS EVE,NOLEN DANTE MAURICE,STEWART NISAN C",
-    "lyricist": "ELLIOTT MELISSA A,JEFFERS EVE,NOLEN DANTE MAURICE,STEWART NISAN C",
-    "release": "2006-10-19"
-  },]
 
 function App() {
-
+  const [data, setData] = useState([]);
   return (
     <>
-      <SearchBar/>
+      <SearchBar setData = {setData}/>
       <SongList data = {data}/>
     </>
   )
--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,13 +1,26 @@
 import './index.css';
+import { getSongs } from '../../apis/song';
+import { useRef } from 'react';
 
-function SearchBar(){
+function SearchBar(props){
+    // Hook 방법
+    const inputRef = useRef();
     const handleSearch = () => {
+        getSongs(inputRef.current.value).then((res) => {
+            props.setData(res.data);
+        })
     }
     return (
         <div className="SearchBar">
           <input
             type="text"
             className="SearchBar__input"
+            ref={inputRef}
+            onKeyDown={(e) => {
+                if (e.key === 'Enter') {
+                    handleSearch();
+                }
+            }}
           />
           <button className="SearchBar__button" onClick={handleSearch}>
             검색
